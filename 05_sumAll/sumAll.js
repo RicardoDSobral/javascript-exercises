941681a8e68855b723ef26ab58d4a50a0b3d0891
RicardoDSobral <ricardoduransobral@gmail.com>
2023-05-14 14:15:24 -0300

    Completes exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(range1, range2) {
+    if (!Number.isInteger(range1) || !Number.isInteger(range2)){
+        return "ERROR";
+    }
+    let sum = 0;
 
+    if (range1 < 0 || range2 < 0){
+        return "ERROR";
+    }
+    else if (range1 < range2){
+        for (let i = range1; i <= range2; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    else if (range1 > range2){
+        for (let i = range2; i <= range1; i++){
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
